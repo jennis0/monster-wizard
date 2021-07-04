4f50c1c866ff4162743bd5da071cf96f3a6ad542
Joe <joe.ennis0@gmail.com>
2021-07-04 15:04:31 +0100

    Monkey patch special character parsing in PDFMiner to fix missing ligatures

--- data_loaders/pdf_loader.py
+++ data_loaders/pdf_loader.py
@@ -10,6 +10,7 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTChar
 from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdffont import PDFUnicodeNotDefined
 
 from pdf2image import convert_from_path
 
@@ -22,6 +23,51 @@ from data_loaders.data_loader_interface import DataLoaderInterface
 from utils.datatypes import Line, Bound, Source, Section
 
 from typing import List, Union, Any
+
+FONT_OVERRIDES = {
+	"Calibri": {
+		332:"ft",
+		415:"t",
+		425:"t",
+		427:"t",
+		976:'f'
+	},
+	"Cambria": {
+		332:"ft",
+		415:"t",
+		425:"t",
+		427:"t",
+		976:'f'
+	}
+}
+
+### PDFMiner has been found to have some issues with rendering ligatures in at least one 
+### pdf I tested. Here we manually override the character processing function so we can
+### easily inject our own stuff - without having to modify the base library
+def override_render_char(self, matrix, font, fontsize, scaling, rise, cid, ncs,
+					graphicstate):
+		try:
+			text = font.to_unichr(cid)
+			assert isinstance(text, str), str(type(text))
+		except PDFUnicodeNotDefined:
+			text = self.handle_undefined_char(font, cid)
+		textwidth = font.char_width(cid)
+		textdisp = font.char_disp(cid)
+
+		font_title = font.basefont.split("-")[0]
+
+		if text == "\x00":
+			if font_title in FONT_OVERRIDES and cid in FONT_OVERRIDES[font_title]:
+				#print("Override {} for font {}".format(cid, font.basefont))
+				text = FONT_OVERRIDES[font_title][cid]
+			else:
+				print("Failed to override {} for font {}".format(cid, font))
+
+		item = LTChar(matrix, font, fontsize, scaling, rise, text, textwidth,
+					  textdisp, ncs, graphicstate)
+		self.cur_item.add(item)
+
+		return item.adv
 
 class PDFLoader(DataLoaderInterface):
 
@@ -54,7 +100,12 @@ class PDFLoader(DataLoaderInterface):
 			laparams = LAParams(all_texts=True)
 
 			device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+			### Hacky monkey patching
+			device.render_char = lambda *args, **kwargs: override_render_char(device, *args, **kwargs)
+
 			interpreter = PDFPageInterpreter(rsrcmgr, device)   
+			
+
 
 			line_id = 0
 
@@ -62,6 +113,7 @@ class PDFLoader(DataLoaderInterface):
 			for page in PDFPage.create_pages(document):
 				page_text = Section()
 				interpreter.process_page(page)
+
 				y_size = page.mediabox[3]
 				x_size = page.mediabox[2]
 				layout = device.get_result()
@@ -122,6 +174,11 @@ class PDFLoader(DataLoaderInterface):
 		elif isinstance(lt, LTTextLine):
 			bound = Bound(lt.bbox[0] / x_size, (y_size - lt.bbox[1]) / y_size, 
 						(lt.bbox[2] - lt.bbox[0]) / x_size, (lt.bbox[3] - lt.bbox[1])/y_size)
+
+			# if lt.get_text().startswith("Art"):
+			# 	print(lt)
+			# 	for o in lt._objs:
+			# 		print(repr(o))
 
 			text = unicodedata.normalize('NFKD', lt.get_text().strip())
 			### Hack to deal with some title being weirdly encoded
